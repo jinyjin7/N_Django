8c7581534fdf76632fe9ab41b47cea4f69bd9925
sojinpark <psjin977@bubblecon.io>
2025-05-28 15:16:01 +0900

    model 수정사항 반영 및 gitignore 수정

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from accounts.models import *
 
 admin.site.register(BaseUser)
+admin.site.register(Teacher)
+admin.site.register(Student)
 admin.site.register(TeacherProfile)
 admin.site.register(StudentProfile)
 admin.site.register(School)
--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,6 @@ from django.db import models
 import uuid
 from django.utils.translation import gettext_lazy as _
 
-#Base유저 매니저와 AbstracBase 유저 추가
 class UserManager(BaseUserManager):
     def create_user(self, email, name, **extra_fields):
         if not email:
@@ -30,14 +29,6 @@ class BaseUser(AbstractBaseUser):
     source_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=100,default="user")
-    phone = models.CharField(max_length=20,null=True, blank=True)
-    address = models.CharField(max_length=255,null=True, blank=True)
-    age = models.PositiveIntegerField(null=True, blank=True)
-    intro = models.TextField(blank=True,null=True)
-    profile_image = models.ImageField(blank=True, null=True)
-    GENDER_CHOICES = [('M', '남'), ('F', '여')]
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES,null=True, blank=True)
-
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
@@ -81,13 +72,11 @@ class BaseUser(AbstractBaseUser):
         return 'none'
 
 
-#choices
 class SchoolChoices(models.TextChoices):
     ELEMENTARY = "elementary", _("초등학교")
     MIDDLE = "middle", _("중학교")
     HIGH = "high", _("고등학교")
 
-# OneToOne 모델로 수정
 class School(models.Model):
     class Meta:
         db_table = 'schools'
@@ -99,10 +88,10 @@ class School(models.Model):
     def __str__(self):
         return self.name
 
-
-class StudentProfile(models.Model):
+# 학생,선생님
+class Student(models.Model):
     class Meta:
-        db_table = 'student_profiles'
+        db_table = 'student'
 
     user = models.OneToOneField(BaseUser, on_delete=models.CASCADE, related_name='student_profile')
     school = models.ForeignKey(School, on_delete=models.CASCADE, related_name='students')
@@ -113,9 +102,10 @@ class StudentProfile(models.Model):
         return f"{self.user.name} 학생 - {self.school}"
 
 
-class TeacherProfile(models.Model):
+
+class Teacher(models.Model):
     class Meta:
-        db_table = 'teacher_profiles'
+        db_table = 'teacher'
 
     user = models.OneToOneField(BaseUser, on_delete=models.CASCADE, related_name='teacher_profile')
     school = models.ForeignKey(School, on_delete=models.CASCADE, related_name='teachers')
@@ -123,3 +113,37 @@ class TeacherProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.name} 선생님 - {self.school}"
+
+# 프로필
+class AbstractBaseProfile(models.Model):
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    intro = models.TextField(blank=True, null=True)
+    profile_image = models.ImageField(blank=True, null=True)
+
+    GENDER_CHOICES = [('M', '남'), ('F', '여')]
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class StudentProfile(AbstractBaseProfile):
+    class Meta:
+        db_table = 'student_profile'
+
+    student = models.OneToOneField(Student, on_delete=models.CASCADE, related_name='student_profile')
+
+    def __str__(self):
+        return f"{self.student} 프로필"
+
+
+class TeacherProfile(AbstractBaseProfile):
+    class Meta:
+        db_table = 'teacher_profile'
+
+    teacher = models.OneToOneField(Teacher, on_delete=models.CASCADE, related_name='teacher_profile')
+
+    def __str__(self):
+        return f"{self.teacher} 프로필"
