39d669d7f204b657dfe7f7bdd2aa6ebf522d1ed2
sojinpark <psjin977@bubblecon.io>
2025-05-29 15:53:32 +0900

    url파일의 주석코드 제거

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,7 +11,5 @@ router.register(r'teachers', TeacherList, basename='teacher')
 urlpatterns = [
     #account url 수정
     path('', include('django.contrib.auth.urls')),
-    # path('login/', auth_views.LoginView.as_view(), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', SignupView.as_view(), name='signup'),
 ]+ router.urls
